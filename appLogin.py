3253cbadc21d6b64d37073115f16f20acb2b02a7
我.doc <36567193+idocx@users.noreply.github.com>
2019-06-09 13:35:17 +0800

    网页端登陆、预约组件

--- appLogin.py
+++ appLogin.py
@@ -1,3 +1,10 @@
+#############################################################
+#                        作者：我.doc                        #
+# Github地址：https://github.com/idocx/WHULibSeatReservation #
+#       个人网站：https://www.chemhy.design                  #
+#############################################################
+
+# 网页端的登陆代码
 from urllib import request, parse
 import ssl
 import json
