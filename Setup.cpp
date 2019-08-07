5da49221632706ee2b7fbf0f31d584ade597ac51
SImple PHyC <44836193+Linqiaosong@users.noreply.github.com>
2019-08-07 18:02:18 +0800

    更新了Linux的代码

--- Setup.cpp
+++ Setup.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
+#include<string>
+using namespace std;
 int main()
 {
-    system("pip install pyqt5 requests");
-    system("pause");
+    
+    
+    //****若为Linux用户，将cmd改为bash*****
+    string commandline="cmd";
+    //************************************
+    
+    
+    
+    if(commandline=="cmd")
+    {
+        system("pip install pyqt5 requests");
+        system("pause");
+    }
+    elseif(commandline=="bash")
+    {
+        system("pip3 install pyqt5 requests");
+        system("read -n 1 -p "Press enter to continue..."");
+    }
     return 0;
 }
